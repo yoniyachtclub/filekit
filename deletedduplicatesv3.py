eeeb5c96fe3c926a2df8e4b8d7ff454975a58c38
meghna mahadevan <meghnamahadevan@meghnas-Air.home>
2024-03-27 18:28:47 -0400

    most recent file in progress

--- deletedduplicatesv3.py
+++ deletedduplicatesv3.py
@@ -48,46 +48,41 @@ def CompileLibrary(pathToSearch):
                     #STEP 2, add all filepaths with that filename as a value of the dictionary
                 musicLibrary[filename].append(root) 
 
-
+#STEP 2, move the to-be-deleted duplicates into a folder to be reviewed and deleted
 def DeleteDuplicates(library):
     global numDuplicates #Total number of duplicates
     src = ""
     keepDest = "./keep"
     deleteDest = "./delete"
 
-    # create keep and delete folders
+    #create keep and delete folders
     if not os.path.exists('./keep'):
         os.makedirs('keep')
     if not os.path.exists('./delete'):
         os.makedirs('delete')
 
-  # try: 
+   #try: 
     for filename in library: 
         listpaths = library[filename] #index for filename
         attributesArray = [] #array with attributes for each version of this file
 
-#STEP 3, choose the best filepath based on hierarchy of organization
+
     #Organization system is based on keeping certain folder schemas or choosing the most recent date modified
         if len(listpaths) > 1: #THESE ARE THE ONES WITH DUPLICATES
             numDuplicates = numDuplicates + 1
             maxFileSize = 0
             mostRecentlyModified = 0
             fileIniTunes = False
-         
-        
 
-            #Go through all the filepaths for that file
-            for fp in listpaths: 
-                fullPath = fp + '/' + filename
-                attributes = getFileAttributes(fullPath)
+            #03.27.24 NEXT STEPS LIST!!!!! Start here meghna 
+            #step 1, write the function for file size comparison (create dictionary for filepath ? )
+            #step 2, write the function for date mod comparison (create dictionary for filepath ? )
+            #step 3, integrate those functions into Delete Duplicates
+            #step 4, compare attributes and move the files into the right folders depending on their attributes
+            #step 5, test 
 
-               
 
-                #LOGIC: Is it in itunes?
-                if attributes["iniTunes"]:
-                    fileIniTunes = True #YES it is in itunes
-                    attributes["keep"] = 1
-                    break
+            #Compare the file size, date modified,  
 
             for fp in listpaths:
                 fullPath = fp + '/' + filename
@@ -107,42 +102,30 @@ def DeleteDuplicates(library):
                         if attributes["lastModified"] > mostRecentlyModified:
                             continue #add code here
                 else:
-            #For this filepath 
-                    
-                    #move this file to delete folder
+    
 
+             #Go through all the filepaths for that file and move this file to delete folder
+            for fp in listpaths: 
+                fullPath = fp + '/' + filename
+                attributes = getFileAttributes(fullPath)
 
                 # check if in itunes - if yes, keep
             attributesArray.sort(key=sortByFileSize) # https://www.w3schools.com/python/ref_list_sort.asp
             print ("sorted by file size:")
             print (attributesArray)
-
-            # check if first two+ elements are same file size. if no, continue with move operation
-            maxFileSize = attributesArray[0]["fileSize"]
-            subAttr = [] #create array with attributes of any same-sized files
-            for attr in attributesArray:
-                if attr["fileSize"] == maxFileSize:
-                    subAttr.append(attr)
-                else:
-                    break
-            # if any of the files are the same size as the max, re-sort those by last modified
-            if len(subAttr) > 1:        
-                subAttr.sort(key=sortByLastModified)
-                src = subAttr[0]["filePath"] # choose most recently modified to keep
-            else:
-                src = attributesArray[0]["filePath"]
-            shutil.move(src,keepDest)
-
+                    
+                
+            
             #TODO move rest to delete folder
         else: 
             src = listpaths[0] + '/' + filename
             attributes = getFileAttributes(src)
             # if not attributes["iniTunes"] : # if no duplicates and not in itunes, move to keep folder.
-            #     shutil.move(src,keepDest)
+            #shutil.move(src,keepDest)
             
     return
 
-
+#Create dictionary to describe each file
 def getFileAttributes(filePath) :
     attributes = {
         "filePath" : filePath,
@@ -182,19 +165,32 @@ def getFileAttributes(filePath) :
 
 # functions for sorting the array of duplicates
 def sortByFileSize(element) : 
+
+    # check if first two+ elements are same file size. if no, continue with move operation
+            maxFileSize = attributesArray[0]["fileSize"]
+            subAttr = [] #create array with attributes of any same-sized files
+            for attr in attributesArray:
+                if attr["fileSize"] == maxFileSize:
+                    subAttr.append(attr)
+                else:
+                    break
+            # if any of the files are the same size as the max, re-sort those by last modified
+            if len(subAttr) > 1:        
+                subAttr.sort(key=sortByLastModified)
+                src = subAttr[0]["filePath"] # choose most recently modified to keep
+            else:
+                src = attributesArray[0]["filePath"]
+            shutil.move(src,keepDest)
+
     return element["fileSize"]
+
 
 def sortByLastModified(element) : 
     return element["lastModified"]
 
-###RUNNING FUNCTIONS
-# search computer
-CompileLibrary('/users/meghnamahadevan/documents/allmusic')
-# search USB(s)
-# CompileLibrary('path/to/usb')
+####
 
-DeleteDuplicates(musicLibrary)
-print("WE HAVE " + str(numDuplicates) + " DUPLICATES TO DEAL WITH OUT OF " + str(totalMusicFiles) + " FILES")
+
 
 
 try:
@@ -207,6 +203,17 @@ except:
         for key in musicLibrary.keys():
             f.write("%s, %s\n" % (key, musicLibrary[key]))
 
+
+
+###RUNNING FUNCTIONS
+# search computer
+CompileLibrary('/users/meghnamahadevan/documents/allmusic')
+DeleteDuplicates(musicLibrary)
+print("WE HAVE " + str(numDuplicates) + " DUPLICATES TO DEAL WITH OUT OF " + str(totalMusicFiles) + " FILES")
+# search USB(s)
+# CompileLibrary('path/to/usb')
+
+
 #TO DO LIST
 #test iTunes feature, is this possible
 #double check file priorities
@@ -218,68 +225,8 @@ except:
 
 
 
-   # except:
- #       pass
-                
-                #ATTRIBUTES
-                # iniTunes = #yes/no - in file path 
-                # recentlymodified = os.stat() #date
-                # recentlyopened= #date
-                # filesize = #size
-                # rekordbox = #date
-                # source = #canifindoutwhereitsdownloadedfrom?
-                
-                #IF THERE IS ONE IN ITUNES
-                    #pass
-
-                #IF THERE ARE MULTIPLE IN ITUNES
-                    #that's okay
-
-                #IF NONE IN ITUNES
-                    #keep the one with with biggest filesize
-
-                    #keep the one with date most recently modified
-
-                
-#                 for filepath in pathways #go through and look for crates
-#                     if filepath contains "CRATES"
-#                         print ("CRATES")
-#                         for filepath in pathways
-#                             if filepath does not contain crates
-#                                 delete
-#                         musicLibrary[filename] = filepath
-#                     else pass
-#                 for filepath in pathways #go through and look for itunes playlist
-#                     if filepath #in itunes playlist
-#                         print ["Itunes" ]
-#                         for filepath in pathways   
-#                             if filepath does not contain itunes
-#                                 delete
-#                     else pass
-#             #  any other duplicates, order by date and delete the others    
-#             if length(pathways) > 1 and pathways is not a string  ##DUPLICATES_rest of the files
-#                 for filepath in pathways[1:]
-#                         delete
-#                         delete the other files
-
-# #STEP 4, dump the files into a folder which needs to be organized
-
-#                 move into one folder, change filepath
-#             else:
-#                 pass
-
-#             if length(pathways) > 1 OR pathways is not a string #ANYOTHERS? 
-#                 print ("There is something weird", pathways)
-#             else:
-#                 pass
-            
-
-#             filepathstr = pathways[0]
-#             musicLibrary[filename]= filepathstr
-            
-
-
 #     ORGANIZE THE DUMP FOLDER
 
 #     export all itunes playlists into text files
 #     Name folders based off of playlists
+
